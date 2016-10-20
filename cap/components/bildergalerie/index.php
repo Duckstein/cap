49df58b9876c573d3bf739fc0d3e419264e11022
Duckstein <internet@skan-tours.de>
2016-10-20 09:28:24 +0200

    new index
    created new index file w list

--- cap/components/bildergalerie/index.php
+++ cap/components/bildergalerie/index.php
@@ -53,7 +53,8 @@ if($deleteid){
 	';
 	
 }
-	
+
+
 
 
 
@@ -65,29 +66,30 @@ include($_SERVER['DOCUMENT_ROOT']."/cap/include/modules/navigation/actionbar.php
 
 // output
 $content.= '
-			<div class="row">
-				<div class="col-md-12">
-				
-					<div class="panel panel-default">
-						<div class="panel-body">
-							<button class="btn btn-success save">Reihenfolge speichern</button>&nbsp;<span id="response"></span>
-						</div>
-					</div>
-				
-					<ul class="sortable">';
+			<div class="row mt">
+				<div class="col-lg-12">
+					
+					<form class="form-horizontal tasi-form" method="get">
+					<table class="table table-striped table-hover table-condensed" id="offertable">
+						<thead>
+							<tr>
+								<th>id</th>
+								<th class="text-center">Kategorie(n)</th>
+								<th>Bild</th>
+								<th>action</th>
+							</tr>
+						</thead>
+						<tbody>';
 
 
 
 
-/* fetch angebote		https://ssdtutorials.com/courses/sort-table-rows-jquery
-
-with the demo: https://coderwall.com/p/okkzpa/sortable-gallery-to-database
-
+/* fetch angebote
 ----------------- */
 
-$sql = "SELECT *
+$sql = "SELECT id, kategorie, titel, bild
 		FROM STRA_bildergalerie
-		ORDER BY ord ASC";	
+		ORDER BY id DESC";	
 					
 $result = mysql_query($sql,$db);
 
@@ -96,38 +98,44 @@ $result = mysql_query($sql,$db);
 
 // loop thru
 
-
-
-
-while( $array = @mysql_fetch_array( $result ) ){ //download every row into array
-	$id = $array['id'];
-	$photo_name = $array['bild'];
+while($row=mysql_fetch_array($result)){
+	
+	$id =			$row['id'];
+	$titel =		$row['titel'];
+	$kategorie =	$row['kategorie'];
+	$bild =			$row['bild'];
+	
+	if($online==1){
+		$status = '<span class="label label-success"><i class="fa fa-check fa-fw"></i></span>';
+	}else{
+		$status = '<span class="label label-danger"><i class="fa fa-times fa-fw"></i></span>';
+	}
 	
 	$content.= '
-						<li id="item-' .$id. '"><img src="/images/bildergalerie/' .$photo_name. '-t.jpg"></li>';
-	
-#	$content.= '
-#							<tr>
-#								<td>' .$id. '</td>
-#								<td class="text-center">' .$kategorie. '</td>
-#								<td><a href="edit.php?id=' .$id. '" title="edit: ' .$bild. '"><img src="/images/bildergalerie/' .$bild. '.jpg" width="90px"></a></td>
-#								<td>
-#									<div class="btn-group" role="group" aria-label="...">
-#										<a href="edit.php?id=' .$id. '" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
-#										<a href="edit.php?clone=' .$id. '&amp;url=' .$bild. '" class="btn btn-warning"><i class="fa fa-clone"></i></a>
-#										<a href="index.php?trashid=' .$id. '&amp;titel=' .$bild. '" class="btn btn-danger"><i class="fa fa-trash"></i></a>
-#									</div>
-#								</td>
-#							</tr>
-#	';
+							<tr>
+								<th scope="row">' .$id. '</th>
+								<td class="text-center">' .$kategorie. '</td>
+								<td><a href="edit.php?id=' .$id. '" title="edit: ' .$bild. '"><img src="/images/bildergalerie/' .$bild. '.jpg" width="90px"></a></td>
+								<td>
+									<div class="btn-group" role="group" aria-label="...">
+										<a href="edit.php?id=' .$id. '" class="btn btn-primary"><i class="fa fa-pencil"></i></a>
+										<a href="edit.php?clone=' .$id. '&amp;url=' .$bild. '" class="btn btn-warning"><i class="fa fa-clone"></i></a>
+										<a href="index.php?trashid=' .$id. '&amp;titel=' .$bild. '" class="btn btn-danger"><i class="fa fa-trash"></i></a>
+									</div>
+								</td>
+							</tr>
+	';
 	
 }
 
 $content.= '
-					</ul>
+						</tbody>
+					</table>
+					</form>
 						
 				</div>
-			</div>';
+			</div>
+';
 
 
 
